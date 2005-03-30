882139d3b8bcd1e4f0091112f23d54575648624f
Martijn Vermaat <martijn@vermaat.name>
2005-03-30 14:36:26 +0000

    Added naturals datatype.

--- ocaml/church.ml
+++ ocaml/church.ml
@@ -1,9 +1,9 @@
 (*
- Fun with church numerals
+ Fun with Church numerals
  Martijn Vermaat, 2005-03-16
 *)
 
-(* The church numeral denoting the integer value 0. *)
+(* The Church numeral denoting the integer value 0. *)
 let zero = fun s -> fun z -> z;;
 
 (*
@@ -13,25 +13,30 @@ let zero = fun s -> fun z -> z;;
  let zero s z = z;;
 *)
 
-(* Some functions on church numerals. *)
+(* Some functions on Church numerals. *)
 let succ = fun x -> fun s -> fun z -> s (x s z);;
 let add  = fun x -> fun y -> fun s -> fun z -> x s (y s z);;
 let mul  = fun x -> fun y -> fun s -> x (y s);;
 
-(* Some more church numerals. *)
+(* Some more Church numerals. *)
 let one = succ zero;;
 let two = succ one;;
 let three = succ two;;
 let four = succ three;;
 let five = succ four;;
 
-(* show returns integer value denoted by the given church numeral. *)
+(* show returns integer value denoted by the given Church numeral. *)
 let show n =
   let inc n = n + 1 in
   n inc 0;;
 
 
-let n = mul (add one two) (add four five)
-in print_int (show n);;
+(* Asside: These Church numerals could also be
+   implemented in their own type. *)
 
+
+(* Test our Church numerals. *)
+
+print_string "Should print (1+2)*(4+5)=27:\n";;
+print_int (show (mul (add one two) (add four five)));;
 print_newline ();;
