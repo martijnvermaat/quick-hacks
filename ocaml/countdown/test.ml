9e0503e41f30d3e2d3d75fcf922a73a8c0d8ad8a
Martijn Vermaat <martijn@vermaat.name>
2005-04-20 10:24:33 +0000

    Wat testjes toegevoegd.

---
+++ ocaml/countdown/test.ml
@@ -0,0 +1,16 @@
+let rec sum n =
+  if n = 0 then []
+  else n::(sum (n-1))
+
+let rec take n l = match n with
+    0 -> []
+  | n -> match l with
+        []    -> raise (Failure "take")
+      | x::xs -> x::(take (n-1) xs)
+
+let rec subs l = match l with
+    []    -> [[]]
+  | x::xs ->
+      let len = List.length l in
+        (List.map (fun n -> take n l) (List.rev (sum len)))
+        @ (subs xs)
