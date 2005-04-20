c91b5f769a82a127ef3c59e34fa75ae3e3fffd33
Martijn Vermaat <martijn@vermaat.name>
2005-04-20 15:18:36 +0000

    Wat testjes voor countdown toegevoegd.

--- ocaml/countdown/test.ml
+++ ocaml/countdown/test.ml
@@ -8,9 +8,15 @@ let rec take n l = match n with
         []    -> raise (Failure "take")
       | x::xs -> x::(take (n-1) xs)
 
-let rec subs l = match l with
+let rec sublists l = match l with
     []    -> [[]]
   | x::xs ->
       let len = List.length l in
         (List.map (fun n -> take n l) (List.rev (sum len)))
-        @ (subs xs)
+        @ (sublists xs)
+
+let rec insertions e l = match l with
+    []    -> [[e]]
+  | x::xs -> (e::x::xs) :: (List.map (fun l -> x::l) (insertions e xs))
+
+let rec permutations = true
