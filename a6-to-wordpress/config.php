05059bfbdade92a0bbd506f9b59d32acc56ae66c
Martijn Vermaat <martijn@vermaat.name>
2005-04-22 15:18:32 +0000

    Removed dummy password from config.

--- a6-to-wordpress/config.php
+++ a6-to-wordpress/config.php
@@ -7,7 +7,7 @@ header('Content-Type: text/plain; charset=UTF-8');
 
 $db_a6['Host']     = 'localhost';
 $db_a6['User']     = 'root';
-$db_a6['Password'] = 'harm';
+$db_a6['Password'] = '';
 $db_a6['Database'] = 'a6';
 $db_a6['Server']   = 'mysql';
 $db_a6['PConnect'] = false;
@@ -15,7 +15,7 @@ $db_a6['PConnect'] = false;
 
 $db_wp['Host']     = 'localhost';
 $db_wp['User']     = 'root';
-$db_wp['Password'] = 'harm';
+$db_wp['Password'] = '';
 $db_wp['Database'] = 'wordpress';
 $db_wp['Server']   = 'mysql';
 $db_wp['PConnect'] = false;
