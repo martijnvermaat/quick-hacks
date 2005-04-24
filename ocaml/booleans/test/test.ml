a003b42a51cae19728a42ba72a241890ca2b5295
Martijn Vermaat <martijn@vermaat.name>
2005-04-24 14:13:22 +0000

    Unit tests for booleans.

--- ocaml/booleans/test/test.ml
+++ ocaml/booleans/test/test.ml
@@ -1,8 +1,40 @@
+(*
+  Automated tests for booleans datatype.
+*)
+
+
 open OUnit
+open Booleans
 
-let test_empty_list _ =
-  assert_equal [1] ([] @ [])
 
-let test_suite = "List testing" >::: ["test_empty_list" >:: test_empty_list]
+let test_and _ =
+  assert_equal (and' true' true') true';
+  assert_equal (and' true' false') false';
+  assert_equal (and' false' true') false';
+  assert_equal (and' false' false') false'
+
+
+let test_or _ =
+  assert_equal (or' true' true') true';
+  assert_equal (or' true' false') true';
+  assert_equal (or' false' true') true';
+  assert_equal (or' false' false') false'
+
+
+let test_not _ =
+  assert_equal (not' false') true';
+  assert_equal (not' true') false'
+
+
+let test_confusion _ =
+  assert_bool "true and false" (true' <> false')
+
+
+let test_suite = "booleans" >:::
+  ["and"       >:: test_and;
+   "or"        >:: test_or;
+   "not"       >:: test_not;
+   "confusion" >:: test_confusion]
+
 
 let _ = run_test_tt_main test_suite
