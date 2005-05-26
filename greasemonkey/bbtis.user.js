68fe051e95214a966381b43990d85fcf36ef2fce
Martijn Vermaat <martijn@vermaat.name>
2005-05-26 21:46:44 +0000

    Version 1.0 of BBTis Greasemonkey user script.

--- greasemonkey/bbtis.user.js
+++ greasemonkey/bbtis.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name          BBTis
 // @namespace     http://www.cs.vu.nl/~mvermaat/
-// @description   Blackboard enhanced with TisVU results
+// @description   Blackboard homepage enhanced with latest TisVU results
 // @include       http://bb.vu.nl/*
 // ==/UserScript==
 
 
 /*
+    BBTis
+
+    Version: 1.0, 2005-05-26
+
     This is just a quick hack to see what's possible with
     some Greasemonkey magic and TisVU.
 
@@ -15,19 +19,24 @@
 
 
 
+/*
+    Wrap the whole thing in an anonymous function to avoid
+    nameclashes with existing Javascript.
+*/
+(function() {
+
+
+
 /***********************************************************************
     Configuration
 ***********************************************************************/
 
 
-var applicationTitle = 'TisNiks';
-
-var maxTisResults = 8;
-
-var urlMainPage = 'http://bb.vu.nl/webapps/portal/tab/_1_1/index.jsp';
-var urlLoginRequest = 'https://tisvu.vu.nl/tis/TI_SEC_PCK.TI_CHECK_LOGON';
-var urlResults = 'https://tisvu.vu.nl/tis/TI001Q01$TUV.QueryList';
-var urlSetCookies = 'https://tisvu.vu.nl/tis/menu';
+var maxTisResults    = 8;
+var autoLogin        = false;
+var tisUser          = '';      /* VU-net-id (if autoLogin=true) */
+var tisPassword      = '';
+var urlMainPage      = 'http://bb.vu.nl/webapps/portal/tab/_1_1/index.jsp';
 
 
 
@@ -43,6 +52,27 @@ function showLoginForm() {
 }
 
 
+function showLoginError() {
+
+    // I admit, this is ugly
+    var s = '<b style="color:#c00">Login failed, try again.</b>';
+
+    document.getElementById('tisError').innerHTML = s;
+
+}
+
+
+function hideLoginError() {
+
+    document.getElementById('tisError').innerHTML = '';
+
+}
+
+
+/*
+    Create a table from the results and put it in place.
+*/
+
 function showResults(results) {
 
     document.getElementById('tisLogin').style.display = 'none';
@@ -52,19 +82,26 @@ function showResults(results) {
 
     table.style.width = '100%';
 
+    // Add some ugly Blackboard HTML for the table header
+    table.innerHTML = '<tr><th colspan="3" align="left" bgcolor="#f0f0f0">'
+        + '<font face="Arial, Helvetica, sans-serif" size="2">'
+        + 'Results from <a href="http://tisvu.vu.nl/">TisVU</a>:</font></th></tr>';
+
     for (var i = 0; i < results.length && i < maxTisResults; i++) {
 
         row = document.createElement('tr');
 
-        for (var j = 2; j < results[i].length; j++) {
+        cell = document.createElement('td');
+        cell.appendChild(document.createTextNode(results[i].date));
+        row.appendChild(cell);
 
-            if (j == 4) continue;
+        cell = document.createElement('td');
+        cell.appendChild(document.createTextNode(results[i].course));
+        row.appendChild(cell);
 
-            cell = document.createElement('td');
-            cell.appendChild(document.createTextNode(results[i][j]));
-            row.appendChild(cell);
-
-        }
+        cell = document.createElement('td');
+        cell.appendChild(document.createTextNode(results[i].result));
+        row.appendChild(cell);
 
         table.appendChild(row);
 
@@ -77,164 +114,221 @@ function showResults(results) {
 
 
 /***********************************************************************
-    Data request functions
+    TisNiks Library
 ***********************************************************************/
 
 
-/*
-    Try to do a login. We show the login form on authorization
-    failure, and the index page on success.
-*/
+var tisNiks = {
 
-function loginAndGetResults(name, password) {
+    /*
+        TisNiks is a collection of functions for querying TisVU.
 
-    function sendLogin() {
+        This version is somewhere near 0.0003 or something.
+    */
 
-        GM_xmlhttpRequest({
-            method:  'POST',
-            url:     urlLoginRequest,
-            data:    'P_USERID=' + name + '&P_PASSWORD=' + password,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-                    },
-            onload:  handleLogin
-        });
 
-    }
+    /*
+        Local variables
+    */
 
-    function handleLogin(details) {
+    tisNiksLog:      false,
 
-        if (details.status == 200) {
+    urlLoginRequest: 'https://tisvu.vu.nl/tis/TI_SEC_PCK.TI_CHECK_LOGON',
+    urlResults:      'https://tisvu.vu.nl/tis/TI001Q01$TUV.QueryList',
+    urlSetCookies:   'https://tisvu.vu.nl/tis/menu',
 
-            /*
-                String 'studieadministratie' is only present in a
-                success response.
-            */
 
-            if (/studieadministratie/i.test(details.responseText)) {
-                //showIndex();
-                GM_log('logged in');
-                getResults();
-            } else {
-                //showLoginForm();
-                //showError('Username/password incorrect.');
-                GM_log('username/password incorrect');
-            }
+    /*
+        Try to do a login. We show the login form on authorization
+        failure, and the index page on success.
+    */
 
-        } else {
+    login: function(name, password, onSuccess, onFailure) {
 
-            GM_log('Could not make login request');
+        var log = this.log;
+        var url = this.urlLoginRequest;
 
+        /*
+            Before trying to login, make a request to TisVU
+            that sets some cookies. If we don't have these
+            cookies, it won't let us login.
+        */
+
+        function doLogin() {
+            GM_xmlhttpRequest({
+                method:  'POST',
+                url:     url,
+                data:    'P_USERID=' + name + '&P_PASSWORD=' + password,
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+                onload:  function(details) {
+
+                    if (details.status == 200) {
+
+                        /*
+                            String 'studieadministratie' is only present in a
+                            success response.
+                        */
+
+                        if (/studieadministratie/i.test(details.responseText)) {
+                            log('logged in');
+                            onSuccess();
+                        } else {
+                            log('username/password incorrect');
+                            onFailure('authorization');
+                        }
+
+                    } else {
+
+                        log('Could not make login request');
+                        onFailure('request');
+
+                    }
+
+                }
+
+            });
         }
 
-    }
+        this.setCookies(doLogin, (function() {}));
 
-    sendLogin();
-
-}
+    },
 
 
-/*
-  Get exam results.
-*/
+    /*
+        Get TisVU results.
+    */
 
-function getResults() {
+    getResults: function(onSuccess, onFailure) {
 
-    function sendResults() {
+        var log = this.log;
 
         GM_xmlhttpRequest({
             method:  'GET',
-            url:     urlResults,
-            onload:  handleResults
-        });
+            url:     this.urlResults,
+            onload:  function(details) {
 
-    }
+                if (details.status == 200) {
 
-    function handleResults(details) {
+                    if (/geen gegevens verkregen/i.test(details.responseText)) {
 
-        if (details.status == 200) {
+                        /*
+                            No results means:
+                            There just are no results, or we're not logged in.
+                        */
 
-            if (/geen gegevens verkregen/i.test(details.responseText)) {
+                        log('No results found');
+                        onFailure('empty');
 
-                /*
-                    No results means:
-                    There just are no results, or we're not logged in.
-                */
+                    } else {
 
-                GM_log('No results found');
-                showLoginForm();
+                        /*
+                            This regular expression will give results like this:
 
-                /*
-                    Let stupid TisVU set some cookies we need before
-                    it lets us login...
-                */
-                setCookies();
+                            {
+                              code:           545324
+                              administration: WI
+                              course:         Course Name
+                              date:           dd-mm-yyyy
+                              result:         9+
+                            }
+                        */
 
-            } else {
+                        var match;
+                        var results = new Array();
 
-                /*
-                    This regular expression will give results like this:
-                    1 - vakcode
-                    2 - administratie
-                    3 - vaknaam
-                    4 - datum (dd-mm-yyyy format, but we should not depend on that!)
-                    5 - cijfer
-                */
+                        while (match = /TARGET="fraVF">([^<]+)<\/A><\/TD><TD ALIGN="LEFT">([^<]+)<\/TD><TD ALIGN="LEFT">([^<]+)<\/TD><TD ALIGN="LEFT">([^>]+)<\/TD><TD ALIGN="LEFT">([^>]+)<\/TD>/g.exec(details.responseText)) {
+                            results.push({
+                                code:           match[1],
+                                administration: match[2],
+                                course:         match[3],
+                                date:           match[4],
+                                result:         match[5]
+                            });
+                        }
 
-                var match;
-                var results = new Array();
+                        log('Received results');
+                        onSuccess(results);
 
-                while (match = /TARGET="fraVF">([^<]+)<\/A><\/TD><TD ALIGN="LEFT">([^<]+)<\/TD><TD ALIGN="LEFT">([^<]+)<\/TD><TD ALIGN="LEFT">([^>]+)<\/TD><TD ALIGN="LEFT">([^>]+)<\/TD>/g.exec(details.responseText)) {
-                    results.push(match);
+                    }
+
+                } else {
+
+                    log('Could not make results request');
+                    onFailure('request');
+
                 }
-
-                showResults(results);
 
             }
 
-        } else {
+        });
 
-            GM_log('Could not make results request');
+    },
 
+
+    /*
+        Request a page on TisVU that sets some cookies.
+    */
+
+    setCookies: function(onSuccess, onFailure) {
+
+        var log = this.log;
+
+        GM_xmlhttpRequest({
+            method:  'GET',
+            url:     this.urlSetCookies,
+            onload:  function(details) {
+
+                if (details.status == 200) {
+
+                    log('Made request for cookies');
+                    onSuccess();
+
+                } else {
+
+                    log('Could not make request for cookies');
+                    onFailure();
+
+                }
+
+            }
+        });
+
+    },
+
+
+    /*
+        Log using Greasemonkey logging function.
+    */
+
+    log: function(message) {
+
+        if (this.tisNiksLog) {
+            GM_log('TisNiks: ' + message);
         }
 
     }
 
-    sendResults();
-
-}
-
-
-/*
-    Request a page on TisVU that sets some cookies.
-*/
-
-function setCookies() {
-
-    GM_xmlhttpRequest({
-        method:  'GET',
-        url:     urlSetCookies,
-    });
 
 }
 
 
 
 /***********************************************************************
-    Main functions
+    Main function
 ***********************************************************************/
 
 
 /*
-    Add our navigation HTML, and build all pages but make
-    them invisible for now.
+    Add the TisVU section, and try to fetch some results.
+    Initialize form handlers and stuff.
 */
 
-function createPage() {
+function bbTis() {
 
     var divTis = document.createElement('div');
     divTis.style.display = 'none';
 
+    // First match will be the left column of My Blackboard sections
     var tds = document.evaluate(
                             "//td[@width='50%']",
                             document,
@@ -248,51 +342,57 @@ function createPage() {
         return;
     }
 
-    var extra = '<table border="0" bgcolor="336699" cellspacing="0" cellpadding="1" width="100%"><tr><td>';
-    extra += '<table border="0" bgcolor="336699" cellspacing="0" cellpadding="2" width="100%"><tr>';
-    extra += '<td bgcolor="336699" width=5><img src="/images/spacer.gif" width=2></td>';
-    extra += '<td width="100%" bgcolor="336699" ><a name="TisVU"></a><span class="moduleTitle">';
-    extra += '<font color =" FFFFFF">TisVU&nbsp;</font></span></td><td align="right" valign="top" width="1%">';
-    extra += '</td></tr></table><table border="0" cellspacing="0" cellpadding="4" width="100%"><tr>';
-    extra += '<td bgcolor="FFFFFF" class="moduleBody"><FONT size=2><p>';
-    extra += 'Your latest results from <a href="http://tisvu.vu.nl">TisVU</a>:';
-    extra += '<form id="tisLogin" style="display:none"><input id="tisuser"><input type="password" id="tispassword">';
-    extra += '<input type="submit" value="login"></form>';
-    extra += '<div id="tisResults"></div>';
-    extra += '</p></FONT></td></tr></table></td></tr></table><br>';
+    // This is the ugly HTML for our TisVU section
+    var s = '<table border="0" bgcolor="336699" cellspacing="0" cellpadding="1" width="100%"><tr><td>'
+        + '<table border="0" bgcolor="336699" cellspacing="0" cellpadding="2" width="100%"><tr>'
+        + '<td bgcolor="336699" width=5><img src="/images/spacer.gif" width=2></td>'
+        + '<td width="100%" bgcolor="336699" ><a name="TisVU"></a><span class="moduleTitle">'
+        + '<font color =" FFFFFF">TisVU&nbsp;</font></span></td><td align="right" valign="top" width="1%">'
+        + '</td></tr></table><table border="0" cellspacing="0" cellpadding="4" width="100%"><tr>'
+        + '<td bgcolor="FFFFFF" class="moduleBody"><FONT size=2><p>'
+        + '<form id="tisLogin" style="display:none"><table width="100%">'
+        + '<tr><th colspan="2" align="left" bgcolor="#f0f0f0"><font face="Arial, Helvetica, sans-serif" size="2">'
+        + 'Please login</font></th></tr>'
+        + '<tr><td><label for="tiuser">VU-net-id:</label></td><td><input id="tisuser"></td></tr>'
+        + '<tr><td><label for="tispassword">Password:</label></td><td><input type="password" id="tispassword"></td></tr>'
+        + '<tr><td></td><td><input type="submit" value="Login"></td></tr></table>'
+        + '<div id="tisError"></div></form>'
+        + '<div id="tisResults"></div>'
+        + '</p></FONT></td></tr></table></td></tr></table><br>';
 
-    divTis.innerHTML = extra;
+    divTis.innerHTML = s;
 
+    // Add handler for submit of login form
     document.getElementById('tisLogin').onsubmit = function() {
-        loginAndGetResults(document.getElementById('tisuser').value,
-                           document.getElementById('tispassword').value);
+        hideLoginError();
+        tisNiks.login(document.getElementById('tisuser').value,
+                      document.getElementById('tispassword').value,
+                      (function()  { tisNiks.getResults(showResults, (function(s) {}) ) }),
+                      (function(s) { showLoginError() })
+                      );
         return false;
     };
 
-    getResults();
+    if (autoLogin) {
 
-    divTis.style.display = '';
+        // Try to fetch results, on failure do an autologin
+        tisNiks.getResults(showResults,
+                           (function(s) {
+                               tisNiks.login(tisUser,
+                                             tisPassword,
+                                             (function()  { tisNiks.getResults(showResults, (function (s) {}) ) }),
+                                             (function(s) { showLoginForm() } ));
+                           }));
 
-}
+    } else {
 
-
-/*
-    Invocation function
-*/
-
-function tisNiks() {
-
-    if (window.location.href == urlMainPage) {
-
-        /*
-            Eventueel eerst even deze pagina opvragen, zodat
-            cookies geset worden door TisVU:
-            https://tisvu.vu.nl/tis/menu
-        */
-
-        createPage();
+        // Try to fetch results, on failure show login form
+        tisNiks.getResults(showResults, (function(s) { showLoginForm() }) );
 
     }
+
+    // Okay, everything is setup, show the thing
+    divTis.style.display = '';
 
 }
 
@@ -303,16 +403,12 @@ function tisNiks() {
 ***********************************************************************/
 
 
-tisNiks();
-
-
-
-/***********************************************************************
-    Documentation
-***********************************************************************/
+if (window.location.href == urlMainPage) {
+    bbTis();
+}
 
 
 /*
-    Comming soon. A lot of my documentation is in the TisNiks
-    userscript.
+    End of wrapper function (see top of script).
 */
+})();
