062722486e7961ef987be55b6a9dd347bf9272d7
Martijn Vermaat <martijn@vermaat.name>
2005-11-09 16:41:12 +0000

    Added optparse to Katja.

--- katja/katja.py
+++ katja/katja.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python
+
+
 """
-Subversion
+Katja, a Gtk Subversion client.
 """
 
 
-VERSION = "0.1.1"
-DATE = "2005/10/25"
+VERSION = "0.1"
+DATE = "2005/11/09"
 
-GLADEFILE = "/usr/lib/nautilus/extensions-1.0/python/subversion.glade"
+GLADEFILE = "katja.glade"
 
 
 from optparse import OptionParser
@@ -43,48 +46,15 @@ def main():
     Parse command line options.
     """
 
-    parser = OptionParser(usage = "usage: %prog <subcommand> [options] "
-                          "[args]",
-                          version = "Subversion %s (%s)" % (VERSION, DATE),
-                          description = "Subversion is a graphical client.")
-
-    parser.add_option("-i", "--input", dest="input", metavar="FILE",
-                      help="read FILE in Fasta format")
-    parser.add_option("-w", "--width", dest="width", metavar="WIDTH",
-                      type="int", help="find motif of width WIDTH")
-    parser.add_option("-t", "--iterations", dest="iterations",
-                      metavar="ITERATIONS", default=ITERATIONS_DEFAULT,
-                      type="int", help="number of non-improving iterations "
-                      "(default " + str(ITERATIONS_DEFAULT) + ")")
-    parser.add_option("-p", "--pseudo", dest="pseudo", metavar="WEIGHT",
-                      default=PSEUDOCOUNTS_WEIGHT_DEFAULT, type="float",
-                      help="use WEIGHT for weight of pseudocounts (default " +
-                      str(PSEUDOCOUNTS_WEIGHT_DEFAULT) + ")")
-    parser.add_option("-s", "--phase-shifts", dest="shifts", metavar="SHIFTS",
-                      default=PHASE_SHIFTS_DEFAULT, type="int",
-                      help="detect phase shifts of width SHIFTS (default " +
-                      str(PHASE_SHIFTS_DEFAULT) + ")")
-    parser.add_option("-f", "--ps-frequency", dest="frequency",
-                      metavar="FREQ", default=PS_FREQUENCY_DEFAULT,
-                      type="int", help="if SHIFTS>0, detect phase shifts "
-                      "every FREQ iterations (default " +
-                      str(PS_FREQUENCY_DEFAULT) + ")")
-    parser.add_option("-n", "--init-num-occurrences", dest="initoccurrences",
-                      metavar="OCCURRENCES",
-                      default=INIT_NUM_OCCURRENCES_DEFAULT, type="int",
-                      help="number of base occurrences to use for initial "
-                      "positions heuristic (default " +
-                      str(INIT_NUM_OCCURRENCES_DEFAULT) + ")")
-    parser.add_option("-v", "--init-pattern-width", dest="initwidth",
-                      metavar="WIDTH", default=INIT_PATTERN_WIDTH_DEFAULT,
-                      type="int", help="if OCCURRENCES>0, width of pattern "
-                      "to use for initial positions heuristic (defaults to "
-                      "value of --width)")
-    parser.add_option("-c", "--cow", action="store_true", dest="cow",
-                      default=False, help="display cow (not recommended)")
+    parser = OptionParser(usage = "usage: %prog <command> [options] [args]",
+                          version = "Katja %s (%s)" % (VERSION, DATE),
+                          description = "Katja is a graphical Subversion "
+                          "client.")
 
     (options, args) = parser.parse_args()
 
+    if len(args) < 1:
+        parser.error("no command provided")
 
     actionCheckout("~")
     gtk.main()
