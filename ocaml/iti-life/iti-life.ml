786c2c6d84cf7b6665aaabbb5c38089e175aa592
Martijn Vermaat <martijn@vermaat.name>
2006-03-01 16:27:43 +0000

    Drawing template.

--- ocaml/iti-life/iti-life.ml
+++ ocaml/iti-life/iti-life.ml
@@ -1,7 +1,10 @@
 open Graphics
 
-let width = 1000
-let height = 800
+let width = 40
+let height = 40
+
+let field_width = 20
+let field_height = 20
 
 (*
 let col = 20
@@ -9,16 +12,39 @@ let lig = 10
 let pas = 30
 *)
 
-let () =
-  open_graph (Printf.sprintf " %dx%d" width height);
-  set_color black
-
-let click (x,y) =
+let click (x, y) =
   ignore (wait_next_event [Button_up]);
-  moveto x y;
-    draw_char (Char.chr (Char.code '0' + 59))
+  let fill_field x y =
+    fill_rect
+      (x * field_width + 1)
+      (y * field_width + 1)
+      (field_width - 2) 
+      (field_height - 2)
+  in
+    fill_field (x / field_width) (y / field_height)
 
-let () = 
+let draw_grid x y =
+  let rec draw_vertical_lines x y =
+    if x > 0 then begin
+      moveto (x * field_width) (y * field_height);
+      lineto (x * field_width) 0;
+      draw_vertical_lines (x - 1) y
+    end
+  in
+  let rec draw_horizontal_lines x y =
+    if y > 0 then begin
+      moveto (x * field_width) (y * field_height);
+      lineto 0 (y * field_height);
+      draw_horizontal_lines x (y - 1)
+    end
+  in
+    draw_vertical_lines x y;
+    draw_horizontal_lines x y
+
+let () =
+  open_graph (Printf.sprintf " %dx%d" (width * field_width) (height * field_height));
+  set_color black;
+  draw_grid width height;
   try
     while true do
       let st = wait_next_event [Button_down; Key_pressed] in
