c151de66e14e633bb32ba994762fbcd817be501e
Martijn Vermaat <martijn@vermaat.name>
2006-03-15 20:02:22 +0000

    Used array and dubbel buffering.

--- ocaml/iti-life/iti-life.ml
+++ ocaml/iti-life/iti-life.ml
@@ -1,48 +1,54 @@
 open Graphics
 
-let width = 32
-let height = 32
+let board_width = 40
+let board_height = 40
 
-let field_width = 18
-let field_height = 18
+let field_width = 20
+let field_height = 20
 
-let click (x, y) =
+
+let get_state board (x,y) = 
+  try
+    board.(x).(y);
+  with Invalid_argument arg ->
+    false
+
+let draw_board board =
+  auto_synchronize false;
+  for x = 0 to board_width - 1 do
+    for y = 0 to board_height - 1 do
+      if (get_state !board (x,y)) then set_color red
+      else set_color black;
+      fill_rect (x * field_width) (y * field_height) field_width field_height
+    done
+  done;
+  auto_synchronize true
+
+let init_board board =
+  !board.(28).(30) <- true;
+  !board.(29).(30) <- true;
+  !board.(29).(31) <- true;
+  !board.(30).(31) <- true;
+  !board.(30).(32) <- true;
+  !board.(31).(32) <- true;
+  !board.(31).(33) <- true;
+  !board.(32).(33) <- true;;
+
+let b = ref (Array.make_matrix board_width board_height false)
+
+let click board (x, y) =
   ignore (wait_next_event [Button_up]);
-  let fill_field x y =
-    fill_rect
-      (x * field_width + 1)
-      (y * field_width + 1)
-      (field_width - 2) 
-      (field_height - 2)
-  in
-    fill_field (x / field_width) (y / field_height)
-
-let draw_grid x y =
-  let rec draw_vertical_lines x y =
-    if x > 0 then begin
-      moveto (x * field_width) (y * field_height);
-      lineto (x * field_width) 0;
-      draw_vertical_lines (x - 1) y
-    end
-  in
-  let rec draw_horizontal_lines x y =
-    if y > 0 then begin
-      moveto (x * field_width) (y * field_height);
-      lineto 0 (y * field_height);
-      draw_horizontal_lines x (y - 1)
-    end
-  in
-    draw_vertical_lines x y;
-    draw_horizontal_lines x y
+  !board.(x / field_width).(y / field_height) <- true;
+  draw_board board
 
 let () =
-  open_graph (Printf.sprintf " %dx%d" (width * field_width) (height * field_height));
-  set_color black;
-  draw_grid width height;
+  open_graph (Printf.sprintf " %dx%d" (board_width * field_width) (board_height * field_height));
+  init_board b;
+  draw_board b;
   try
     while true do
       let st = wait_next_event [Button_down; Key_pressed] in
-        if st.button then click (mouse_pos ());
+        if st.button then click b (mouse_pos ());
         if st.keypressed then begin
           let c = st.key in
 	        if c = 'q' then raise Exit
