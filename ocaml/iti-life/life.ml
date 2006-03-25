cd0ad8d5c8ea91e89067d6604deab80cbe877729
Martijn Vermaat <martijn@vermaat.name>
2006-03-25 17:31:11 +0000

    Implemented functional style game of life.

--- ocaml/iti-life/life.ml
+++ ocaml/iti-life/life.ml
@@ -1,96 +1,78 @@
 open Graphics
+open World
 
-let board_width = 80
-let board_height = 80
+let board_width = 50
+let board_height = 50
 
 let field_width = 10
 let field_height = 10
 
 
-type cell = Living | Dead
-type coordinate = int * int
-(* a cell consists of an (x,y) position and a state (include coordinate?) *)
-(* type cell = coordinate * state *)
-(* board contains coordinates of living cells (include width,height?) *)
-type board = coordinate list
-
-let empty_board = []
-
-let rec remove_from_list pos = function
-    []   -> []
-  | h::t -> if h = pos then t else h::(remove_from_list pos t)
-
-let cell_at pos board =
-  if List.mem pos board then
-    Living
-  else
-    Dead
-
-let toggle_cell pos board =
-  match cell_at pos board with
-      Living -> remove_from_list pos board
-    | Dead   -> pos::board
-
-
-(*
-  Idea:
-  have an *abstract* datatype for the board implemented
-  as a list of coordinates for living cells.
-  operations on the board return cells as cell datatype,
-  a tuple of coordinates and state.
-  for the next round a new board is created.
-  maybe have coordinates (0,0) in the center of the board,
-  to make encoding of interesting figures easy.
-
-  idea stolen from
-  http://homepages.inf.ed.ac.uk/dts/fps/pract3-2004/pract3.sml
-*)
-
-
-let draw_board b =
+let draw_board world =
   auto_synchronize false;
   for x = 0 to board_width - 1 do
     for y = 0 to board_height - 1 do
       begin
-        match cell_at (x, y) b with
-            Living -> set_color red
-          | Dead   -> set_color black
+        match cell_at (x, y) world with
+            Living, _ -> set_color red
+          | Dead, _   -> set_color black
       end;
       fill_rect (x * field_width) (y * field_height) field_width field_height
     done
   done;
   auto_synchronize true;
-  b
+  world
 
-let click board (x, y) =
+let click world (x, y) =
   ignore (wait_next_event [Button_up]);
   let pos = x/field_width, y/field_height in
-    draw_board (toggle_cell pos board)
+    toggle_cell pos world
 
-let evolve_board board =
-  board
+let evolve_state state neighbours =
+  (* this can be nicer *)
+  match state, neighbours with
+      _, 3      -> Living
+    | Living, 2 -> Living
+    | _         -> Dead
 
-let rec main board =
-  let new_board =
+let evolve_world world =
+  (*
+    problem is that world does has no explicit bounds. a map over this world
+    is conceptually fine, but not straightforward to implement
+    solution 1) add width,height to world
+                + simple
+                - ugly
+    solution 2) let it just calculate as far as it wants to, but only use
+                what's inside the board dimensions
+                + simple?
+                - after many rounds there can be many unused cells
+    solution 3) a lazy version of solution 2
+                + elegant
+                - hard?
+  *)
+  world_map (function s, pos -> evolve_state s (neighbours pos world), pos) world
+
+let rec main world =
+  let next_world =
     begin
       let st = wait_next_event [Button_down; Key_pressed] in
         if st.button then
-          click board (mouse_pos ())
+          draw_board (click world (mouse_pos ()))
         else if st.keypressed then
           match st.key with
               'q' -> raise Exit
-            | ' ' -> evolve_board board
-            | _   -> board
+            | ' ' -> draw_board (evolve_world world)
+            | _   -> world
         else
-          board
+          world
     end
   in
-    main new_board
+    main next_world
 
 
 let () =
   open_graph (Printf.sprintf " %dx%d" (board_width * field_width) (board_height * field_height));
   try
-    main empty_board
+    main (new_world board_width board_height)
   with
     | Exit -> exit 0
---
+++ ocaml/iti-life/world.ml
@@ -0,0 +1,100 @@
+type state = Living | Dead
+type coordinate = int * int
+type cell = state * coordinate
+
+(* internal representation *)
+(* a world is conceptually unbounded *)
+(* not any more, width,height is included *)
+type world = int * int * coordinate list
+
+let new_world width height = width, height, []
+
+let rec remove_from_list pos = function
+    []   -> []
+  | h::t -> if h = pos then t else h::(remove_from_list pos t)
+
+(* this should not be used, do away with it *)
+let cell_at pos world =
+  let width, height, cells = world in
+    if List.mem pos cells then
+      Living, pos
+    else
+      Dead, pos
+
+
+let toggle_cell pos world =
+  let width, height, cells = world in
+    if List.mem pos cells then
+      width, height, remove_from_list pos cells
+    else
+      width, height, pos::cells
+
+
+let add_cell cell world =
+  let s, pos = cell in
+  let width, height, cells = world in
+  let cell_removed = remove_from_list pos cells in
+    match s with
+        Living -> width, height, pos::cell_removed
+      | _      -> width, height, cell_removed
+
+
+let neighbours pos world =
+  let x, y = pos in
+  let list_of_neighbours =
+    cell_at (x, y+1) world
+    :: cell_at (x+1, y+1) world
+    :: cell_at (x+1, y) world
+    :: cell_at (x+1, y-1) world
+    :: cell_at (x, y-1) world
+    :: cell_at (x-1, y-1) world
+    :: cell_at (x-1, y) world
+    :: cell_at (x-1, y+1) world
+    :: []
+  in
+    List.fold_left
+      (fun n -> function
+           (Living, _) -> n + 1
+         | _           -> n)  (* isn't there an identity function? *)
+      0
+      list_of_neighbours
+
+
+let rec map_highest_row f width height world =
+  if width > 0 then
+    map_highest_row f (width-1) height (add_cell (f (cell_at (width-1, height-1) world)) world)
+  else
+    world
+
+
+let rec map_all f width height world =
+  if height > 0 then
+    map_all f width (height-1) (map_highest_row f width height world)
+  else
+    world
+
+
+let world_map f world =
+  let width, height, cells = world in
+    map_all f width height world
+
+
+
+
+(*
+  Idea:
+  have an *abstract* datatype for the board implemented
+  as a list of coordinates for living cells.
+  operations on the board return cells as cell datatype,
+  a tuple of coordinates and state.
+  for the next round a new board is created.
+  maybe have coordinates (0,0) in the center of the board,
+  to make encoding of interesting figures easy.
+
+  performance is reasonable but not good. idea for
+  optimization is to only evolve cells that have potential
+  to change (close to living cells).
+
+  idea stolen from
+  http://homepages.inf.ed.ac.uk/dts/fps/pract3-2004/pract3.sml
+*)
