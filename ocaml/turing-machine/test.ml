e495f103c4faa03b28a405c1bd96febf21123820
Martijn Vermaat <martijn@vermaat.name>
2008-09-17 13:21:29 +0000

    Parsing test.

---
+++ ocaml/turing-machine/test.ml
@@ -0,0 +1,9 @@
+let _ =
+  try
+    let lexbuf = Lexing.from_channel (open_in "test.rules") in
+      while true do
+        let result = Parser.main Lexer.token lexbuf in
+          print_int result; print_newline(); flush stdout
+      done
+  with Lexer.Eof ->
+    exit 0
