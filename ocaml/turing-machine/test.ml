37916fcc108b891bb40c08ee09e5291b12fee3e9
Martijn Vermaat <martijn@vermaat.name>
2008-09-21 19:23:18 +0000

    Removed obsolete files.

--- ocaml/turing-machine/test.ml
+++
@@ -1,9 +0,0 @@
-let _ =
-  try
-    let lexbuf = Lexing.from_channel (open_in "test.rules") in
-      while true do
-        let result = Parser.main Lexer.token lexbuf in
-          print_endline "ja!"; flush stdout
-      done
-  with Lexer.Eof ->
-    exit 0
