91bf985ce46a942e52f5629b22359754ae926e0e
Martijn Vermaat <martijn@vermaat.name>
2009-02-15 12:02:39 +0000

    New version 0.3.0 of Nu Compact.

--- greasemonkey/nucompact.user.js
+++ greasemonkey/nucompact.user.js
@@ -9,7 +9,7 @@
 /*
     Nu Compact
 
-    Version: 0.2.2, 2008-10-18
+    Version: 0.3.0, 2009-02-15
 
     http://www.cs.vu.nl/~mvermaat/greasemonkey
 
@@ -29,11 +29,14 @@
     Known bugs:
     * We should also remove the stupid column soundbites on
       the right side of the frontpage.
-    * We should render the entire page black and suggest some
+    * We should render the entire page blank and suggest some
       other source for news.
 
 
     Changelog
+
+    2009-02-15 - 0.3.0
+    * Adapted to new nu.nl layout
 
     2008-10-18 - 0.2.2
     * Added video section
@@ -63,55 +66,12 @@
 (function() {
 
 
-function rewritePicturizedStories() {
+function getLinksFromPicturizedStories() {
 
-    var stories, story, link, links, newLink, header;
-
-    var sections = [
-            {
-                title: 'Algemeen',
-                color: 'a9d9ff',
-                class: 'verloopAlgemeen',
-                icon: 'img/ico_pijl_1.gif'
-            },
-            {
-                title: 'Economie',
-                color: 'ffbdbd',
-                class: 'verloopBeurs',
-                icon: 'images/ico_pijl_2.gif'
-            },
-            {
-                title: 'Internet',
-                color: 'cac6f7',
-                class: 'verloopInternet',
-                icon: 'images/ico_pijl_3.gif'
-            },
-            {
-                title: 'Sport',
-                color: 'bde7bd',
-                class: 'verloopSport',
-                icon: 'images/ico_pijl_4.gif'
-            },
-            {
-                title: 'Overig',
-                color: 'f5e47f',
-                class: 'verloopOverig',
-                icon: 'images/ico_pijl_5.gif'
-            },
-            {
-                title: 'Video',
-                color: 'c4d4ed',
-                class: 'verloopNUvideo',
-                icon: 'images/ico_video.png'
-            }
-    ];
-
-    var xpath_sections = sections.map
-        ( function(s) { return "(@class='" + s.class + "')"; } ).join(" or ");
-    xpath_sections = "//tr[" + xpath_sections + "]";
+    var stories, story, link, links, newLink;
 
     // Find containers for picturized stories
-    stories = document.evaluate(xpath_sections,
+    stories = document.evaluate("//div[contains(@class,'subarticle') or contains(@class, 'leadarticle')]",
                                 document,
                                 null,
                                 XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
@@ -121,79 +81,60 @@ function rewritePicturizedStories() {
 
         story = stories.snapshotItem(i);
 
+        // Get color class
+        cclass = story.className.split(' ').pop();
+
         // The link containing url and title of story
-        link = document.evaluate(".//td[@valign]//a[@class='link']",
+        link = document.evaluate('.//h2/a',
                                  story,
                                  null,
                                  XPathResult.FIRST_ORDERED_NODE_TYPE,
                                  null).singleNodeValue;
 
         // Placeholder for plain links to stories
-        links = document.evaluate("./following::td/div",
+        links = document.evaluate('./following::div[1]//ul',
                                   story,
                                   null,
                                   XPathResult.FIRST_ORDERED_NODE_TYPE,
                                   null).singleNodeValue;
 
-        // Create new link container div
-        newLink = document.createElement('div');
+        // Create new link container li
+        newLink = document.createElement('li');
+        newLink.className = cclass;
 
-        if (i < sections.length) {
-            icon = sections[i].icon;
-        } else {
-            icon = '/img/ico_pijl_1.gif';
-        }
-
-        // Ok, this is ugly
-        newLink.innerHTML =
-            '<b><img src="' + icon + '" border="0" alt=""' + /* width="4" height="7"' +*/
-            ' hspace="4" vspace="0" class="pijl1"> <a class="link" href="' +
-            link.getAttribute('href') + '"> ' + link.firstChild.nodeValue +
-            '</a></b><br>';
+        // Copy the link
+        newLink.insertBefore(link, null);
 
         // Add plain link for this story
         links.insertBefore(newLink, links.firstChild);
-
-        // Empty original section for story (including the picture)
-        while (story.firstChild) {
-            story.removeChild(story.firstChild);
-        }
-
-        // Create a section header
-        if (i < sections.length) {
-            header = document.createElement('td');
-            header.style.background = '#' + sections[i].color;
-            header.style.width = '364px';
-            header.appendChild(document.createTextNode(sections[i].title));
-        }
-
-        // Place section header
-        story.appendChild(header);
 
     }
 
 }
 
 
-function removeUselessHeader() {
+function removeUselessThings() {
 
-    var header;
+    var thing, things;
 
-    // Find the useless header (it contains a td with class 'noprint')
-    header = document.evaluate("//tr[td[@class='noprint']]",
-                               document,
-                               null,
-                               XPathResult.FIRST_ORDERED_NODE_TYPE,
-                               null).singleNodeValue;
+    things = document.evaluate("id('feeds adblock_h masthead')|//div[contains(@class,'subarticle') or contains(@class, 'leadarticle')]",
+                                document,
+                                null,
+                                XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+                                null);
 
-    // Hide it because it's useless
-    header.parentNode.removeChild(header);
+    for (var i = 0; i < things.snapshotLength; i++) {
+
+        thing = things.snapshotItem(i);
+        thing.parentNode.removeChild(thing);
+
+    }
 
 }
 
 
-removeUselessHeader();
-rewritePicturizedStories();
+getLinksFromPicturizedStories();
+removeUselessThings();
 
 
 /*
