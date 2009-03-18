b5169008be9259d2da16225c4c0a2881514cb73c
Martijn Vermaat <martijn@vermaat.name>
2009-03-18 13:53:50 +0000

    Translated archive pages.

--- wordpress-theme-vita/branches/jannylog/archive.php
+++ wordpress-theme-vita/branches/jannylog/archive.php
@@ -6,62 +6,63 @@
       <?php if (have_posts()) : ?>
       <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
       <?php /* If this is a category archive */ if (is_category()) { ?>
-      <h2 class="pagetitle">Archive for the &#8216;<?php echo single_cat_title(); ?>&#8217;</h2>
+      <h2 class="pagetitle">Archief voor &#8216;<?php echo single_cat_title(); ?>&#8217;</h2>
       <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-      <h2 class="pagetitle">Archive for
+      <h2 class="pagetitle">Archief voor
         <?php the_time('F jS, Y'); ?>
       </h2>
       <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-      <h2 class="pagetitle">Archive for
+      <h2 class="pagetitle">Archief voor
         <?php the_time('F, Y'); ?>
       </h2>
       <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-      <h2 class="pagetitle">Archive for
+      <h2 class="pagetitle">Archief voor
         <?php the_time('Y'); ?>
       </h2>
       <?php /* If this is an author archive */ } elseif (is_author()) { ?>
-      <h2 class="pagetitle">Author Archive</h2>
+      <h2 class="pagetitle">Schrijversarchief</h2>
       <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-        <h2 class="pagetitle">Blog Archives</h2>
+        <h2 class="pagetitle">Weblogarchieven</h2>
         <?php } ?>
       <div class="navigation">
         <div class="alignleft">
-          <?php next_posts_link('&larr; Previous Entries') ?>
+          <?php next_posts_link('&larr; Vorige berichten') ?>
         </div>
         <div class="alignright">
-          <?php previous_posts_link('Next Entries &rarr;') ?>
+          <?php previous_posts_link('Volgende berichten &rarr;') ?>
         </div>
       </div>
       <br class="clear" />
       <?php while (have_posts()) : the_post(); ?>
       <div class="post" id="post-<?php the_ID(); ?>">
-        <h2 class="post-title"><a class="intitle" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
+        <h2 class="post-title"><a class="intitle" href="<?php the_permalink() ?>" rel="bookmark" title="Permanente link naar <?php the_title(); ?>">
           <?php the_title(); ?>
           </a> <span class="commr">
           <?php comments_popup_link('0', '1', '%'); ?>
           </span></h2>
-        <div class="timr"> Posted on
+        <div class="timr"> Geplaatst op
           <?php the_time('F d, Y') ?>
-          by
+          door
           <?php the_author() ?>
           <?php edit_post_link('e', '<span class="editr">[', '] </span>'); ?>
         </div>
         <div class="entry">
-          <?php the_content('Read the rest of this entry &rarr;'); ?>
+          <?php the_content('Lees de rest van dit bericht &rarr;'); ?>
         </div>
         <p class="postmetadata">
-          <?php the_tags('Tags: ','','<br />');?>
-          <span class="catr">Category
+          <?php the_tags('Onderwerpen: ','','<br />');?>
+<!--          <span class="catr">Categorie
           <?php the_category(', ') ?>
-          </span> </p>
+          </span> -->
+        </p>
       </div>
       <?php endwhile; ?>
       <div class="navigation">
         <div class="alignleft">
-          <?php next_posts_link('&laquo; Previous Entries') ?>
+          <?php next_posts_link('&laquo; Vorige berichten') ?>
         </div>
         <div class="alignright">
-          <?php previous_posts_link('Next Entries &raquo;') ?>
+          <?php previous_posts_link('Volgende berichten &raquo;') ?>
         </div>
       </div>
       <?php else : ?>
--- wordpress-theme-vita/branches/jannylog/archives.php
+++ wordpress-theme-vita/branches/jannylog/archives.php
@@ -9,21 +9,21 @@ Template Name: Archives
   <div id="content">
     <div class="gap archives post">
       <div class="arlist clearfix">
-        <h2>Archives</h2>
-        <h3>By Month:</h3>
+        <h2>Archieven</h2>
+        <h3>Per maand:</h3>
         <ul>
           <?php wp_get_archives('type=monthly&show_post_count=1') ?>
         </ul>
       </div>
-      <div class="arlist clearfix">
-        <h3>By Subject:</h3>
+<!--      <div class="arlist clearfix">
+        <h3>Per categorie:</h3>
         <ul>
           <?php wp_list_categories('title_li=&hierarchical=0&show_count=1') ?>
         </ul>
-      </div>
+      </div> -->
       <?php if (function_exists('wp_tag_cloud')) { ?>
       <div id="archivebox">
-        <h3>Popular Tags</h3>
+        <h3>Per onderwerp:</h3>
         <ul class="list1">
           <?php wp_tag_cloud('smallest=10&largest=18'); ?>
         </ul>
