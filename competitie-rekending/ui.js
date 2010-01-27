051cfdbf34c44686cd2d2256a912f8a084948014
Martijn Vermaat <martijn@vermaat.name>
2010-01-27 15:40:14 +0000

    Added bookmarking.

--- competitie-rekending/ui.js
+++ competitie-rekending/ui.js
@@ -3,23 +3,32 @@ $(document).ready(function() {
 
     var TO_POINTS = 0;
     var TO_PERFORMANCE = 1;
+
     var direction = TO_POINTS;
+    var sum = 0;
 
 
     var calculate = function() {
 
         if (direction == TO_POINTS) {
             var points = calculator.points($('#events').val(),
-                                             $('#male').is(':checked'),
-                                             $('#performance').val());
+                                           $('#male').is(':checked'),
+                                           $('#performance').val());
             if (!isNaN(points))
                 $('#points').val(points.toString());
         } else {
-            $('#performance').val(calculator.performance(
-                $('#events').val(),
-                $('#male').is(':checked'),
-                $('#points').val()));
+            $('#performance').val(
+                calculator.performance($('#events').val(),
+                                       $('#male').is(':checked'),
+                                       $('#points').val()));
         }
+
+        if (isNaN(parseInt($('#events').val()))
+            || $('#points').val() == ''
+            || $('#performance').val() == '')
+            $('#bookmark').hide();
+        else
+            $('#bookmark').show();
 
     };
 
@@ -36,6 +45,41 @@ $(document).ready(function() {
     };
 
 
+    var bookmark = function() {
+
+        var r = $('<a href="#" class="remove">Verwijder dit resultaat</a>');
+        r.attr('title', r.text()).click(function() {
+            $(this).parent().remove();
+            updateSum(-parseInt($('#points').val())); // TODO: this doesn't use current value of points
+            return false;
+        });
+
+        var sex = $('#male').is(':checked') ? 'm' : 'v';
+        var s = $('#events option:selected').text() + ' (' + sex + '): ' + $('#performance').val() + ', ' + $('#points').val();
+
+        $('#bookmarks').append(
+            $('<li>').text(s).prepend(r)
+        );
+
+        updateSum(parseInt($('#points').val()));
+
+    };
+
+
+    var updateSum = function(points) {
+
+        sum += points;
+        var count = $('#bookmarks li').length;
+
+        if (count > 1) {
+            $('#sum').text(count + ' resultaten met een totaal van ' + sum + ' punten.').show();
+        } else {
+            $('#sum').hide();
+        }
+
+    }
+
+
     var events = calculator.events();
     for (i = 0; i < events.length; i++)
         $('#events').append($('<option>').val(i).text(events[i]));
@@ -46,6 +90,10 @@ $(document).ready(function() {
 
     $('#performance').keyup(to_points);
     $('#points').keyup(to_performance);
+
+    $('#bookmark').click(function() { bookmark(); return false; }).hide();
+
+    $('#sum').hide();
 
 
 });
