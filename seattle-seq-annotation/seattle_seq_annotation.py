f353d0147407f46fb809a609490f6fa788738769
Martijn Vermaat <martijn@vermaat.name>
2011-02-10 13:40:02 +0100

    Refactoring after discovery of web server automated mode.
    - Results are now in plain-text instead of gzipped.
    - A temporary copy of the VCF file is used with a header line.
    - The HTML results tabel is not used anymore.

--- seattle-seq-annotation/seattle_seq_annotation.py
+++ seattle-seq-annotation/seattle_seq_annotation.py
@@ -3,23 +3,24 @@
 # Annotate variants using SeattleSeq Annotation.
 #
 # Given a VCF file with variants, submit the file to the SeattleSeq Annotation
-# web interface [1]. The annotation result is retrieved as gzipped
+# web interface [1]. The annotation result is retrieved as plain-text
 # tab-separated file.
 #
 # Usage:
 #   ./seattle_seq_annotation.py <input.vcf>
 #
-# The result is written to disk as <input.vcf.annotation.gz>.
+# The result is written to disk as <input.vcf.annotation>. Temporary data is
+# written to <input.vcf.annotation.tmp> and removed afterwards.
 #
 # Requires the poster Python library [2].
 #
 # [1] http://gvs.gs.washington.edu/SeattleSeqAnnotation131/
 # [2] http://atlee.ca/software/poster/
 #
-# 2011-02-09, Martijn Vermaat <m.vermaat.hg@lumc.nl>
+# 2011-02-10, Martijn Vermaat <m.vermaat.hg@lumc.nl>
 
 
-# E-mail address to use in submit (job result will be mailed there)
+# E-mail address to use in submit (job errors will be mailed here)
 EMAIL = 'm.vermaat.hg@lumc.nl'
 
 # Seconds to wait between polling for job result
@@ -50,9 +51,13 @@ COLUMNS = ['sampleAlleles',
 #           'microRNAs',
            'clinicalAssociation']
 
+# SeattleSeq Annotation location
+BASE_URL = 'http://gvs.gs.washington.edu/SeattleSeqAnnotation131/'
+POST_URL = BASE_URL + 'BatchQueryServlet'
+
 
 import sys
-import re
+import os
 import time
 from poster.encode import multipart_encode
 from poster.streaminghttp import register_openers
@@ -60,44 +65,35 @@ import urllib
 import urllib2
 
 
-# Some ugly urls
-BASE_URL = 'http://gvs.gs.washington.edu/SeattleSeqAnnotation131/'
-POST_URL = BASE_URL + 'BatchQueryServlet'
-MONITOR_URL_SCHEME = BASE_URL + 'BatchProgressServlet?progressFile=' \
-                     + 'AnnotationProgress.{job}.txt&submittedFile={file}&' \
-                     + 'cancelFile=AnnotationCancel.{job}.txt&isVCFIn=true&' \
-                     + 'isVCFOut=false'
-TABLE_URL_SCHEME = BASE_URL + 'AnnotationReadBackServlet?clickType=direct&' \
-                   + 'progressFile=AnnotationProgress.{job}.txt'
-RESULT_URL_SCHEME = 'http://gvs-p.gs.washington.edu/SeattleSeqAnnotation131' \
-                    + '/BatchFileDownloadServlet?file={file}.gz&download=true'
-
-
 def seattle_seq_annotation(vcf_file):
     """
     Submit a VCF file to the SeattleSeq Annotation web interface. The
-    annotation result is retrieved as gzipped tab-separated and written to
-    disk.
+    annotation result is retrieved as plain-text tab-separated and written
+    to disk.
+
+    @todo: VCF file must contain '# autoFile testAuto.txt' as first line.
     """
-    job_id, submitted_file = submit_vcf_file(vcf_file)
-    wait_for_result(job_id, submitted_file)
-    result_file = get_result_file(job_id, submitted_file)
-    write_result_file(result_file, vcf_file + '.annotation.gz')
+    monitor_url, result_url = submit_vcf_file(vcf_file)
+    wait_for_result(monitor_url)
+    write_result_file(result_url, vcf_file + '.annotation')
 
 
 def submit_vcf_file(vcf_file):
     """
     Submit a VCF file to the SeattleSeq Annotation server and return as a
-    tuple the job id and the name of the submitted file.
+    tuple the url to monitor the job and the url to get the result.
     """
+    auto_file = vcf_file + '.annotation.tmp'
+    create_auto_file(vcf_file, auto_file)
+
     try:
-        vcf = open(vcf_file, 'r')
+        auto = open(auto_file, 'r')
     except IOError as (_, message):
         fatal_error(message)
 
     parameters = [('genotypeSource',   'FileInput'),
                   ('EMail',            EMAIL),
-                  ('GenotypeFile',     vcf),
+                  ('GenotypeFile',     auto),
                   ('fileFormat',       'VCF'),
                   ('outputFileFormat', 'original'),
                   ('geneData',         'NCBI'),
@@ -106,59 +102,78 @@ def submit_vcf_file(vcf_file):
     for column in COLUMNS:
         parameters.append( ('columns', column) )
 
-    submit = post_multipart(POST_URL, parameters)
-    submit_url = submit.geturl()
+    # Response contains result url and monitor url separated by a comma
+    response = post_multipart(POST_URL, parameters)
+    urls = response.readline().strip().split(',')
+    response.close()
 
-    debug('Submit url: %s' % submit_url)
+    auto.close()
+    os.unlink(auto_file)
 
-    # Get job id from url
-    match = re.search(r'progressFile=AnnotationProgress\.(\d+)\.txt',
-                      submit_url)
-    if not match:
-        fatal_error('Could not find job id in submit url.')
-    job_id = match.group(1)
+    if not len(urls) == 2:
+        fatal_error('Could not read urls from submit response.')
 
-    # Get submitted file from url
-    match = re.search(r'submittedFile=([^=&]+)',
-                      submit_url)
-    if not match:
-        fatal_error('Could not find submitted file in submit url.')
-    submitted_file = match.group(1)
+    result_url, monitor_url = urls
 
-    debug('Job id: %s' % job_id)
-    debug('Submitted file: %s' % submitted_file)
+    debug('Monitor url: %s' % monitor_url)
+    debug('Result url: %s' % result_url)
 
-    return job_id, submitted_file
+    return monitor_url, result_url
 
 
-def wait_for_result(job_id, submitted_file):
+def create_auto_file(vcf_file, auto_file):
+    """
+    Copy the VCF file to another file, prepending it with a line needed by
+    the SeattleSeq Annotation server.
+    """
+    try:
+        vcf = open(vcf_file, 'r')
+    except IOError as (_, message):
+        fatal_error(message)
+
+    try:
+        auto = open(auto_file, 'w')
+    except IOError as (_, message):
+        fatal_error(message)
+
+    # Header line for automated processing
+    auto.write('# autoFile testAuto.txt\n')
+
+    # Buffered read/write
+    while True:
+        line = vcf.readline()
+        if not line:
+            break
+        auto.write(line)
+
+    debug('Copied VCF file to temporary file.')
+
+    vcf.close()
+    auto.close()
+
+
+def wait_for_result(monitor_url):
     """
     Monitor the job for progress. Return when the job is completed.
     """
-    monitor_url = MONITOR_URL_SCHEME.format(job=job_id, file=submitted_file)
-
-    debug('Monitor url: %s' % monitor_url)
-
     waiting = 0
     while True:
         if waiting > WAIT_MAX:
-            fatal_error('Job took too long (monitor url: %s)' % monitor_url)
+            fatal_error('Job took too long.')
 
         time.sleep(WAIT_STEP)
         waiting += WAIT_STEP
 
-        monitor = get(monitor_url).read()
+        # Response contains number of variants processed and total number of
+        # variants separated by a comma
+        response = get(monitor_url)
+        monitor = response.readline().strip().split(',')
+        response.close()
 
-        # Check if we have a monitor page that looks sane
-        if monitor.find('in your file have been processed') == -1:
-            fatal_error('Could not monitor job (monitor_url: %s)'
-                        % monitor_url)
+        if not len(monitor) == 2:
+            fatal_error('Could not read progress from monitor.')
 
-        # Find a progress > 0% (the message is different in that case)
-        progress = re.search(r'(\d+) out of (\d+) variations', monitor)
-        if not progress:
-            debug('Waiting: 0%')
-            continue
+        processed, total = monitor
 
         # Todo: If we submit a file with 0 variants, the monitor page keeps
         # saying '0 variations in your file have been processed', even after
@@ -167,7 +182,10 @@ def wait_for_result(job_id, submitted_file):
         # only solution I can think of is checking the input file for this
         # special case.
 
-        processed, total = progress.group(1), progress.group(2)
+        # If no variants have yet been processed, we always get 0,0
+        if total == '0':
+            debug('Waiting: 0%')
+            continue
 
         debug('Waiting: %s / %s' % (processed, total))
 
@@ -176,58 +194,27 @@ def wait_for_result(job_id, submitted_file):
             break
 
 
-def get_result_file(job_id, submitted_file):
+def write_result_file(result_url, output_file):
     """
-    For a (completed) job, get the result file name.
-
-    It is unfortunate that we can only get the location of the plain text
-    result file from the HTML table (okay, also from the e-mail).
-    So we read in the HTML table (big) and construct the url to the plain
-    text result.
+    Get plain-text result from server and write it to a file.
     """
-    table_url = TABLE_URL_SCHEME.format(job=job_id, file=submitted_file)
-
-    debug('Table url: %s' % table_url)
-
-    table = get(table_url)
-
-    result_file = None
-    while True:
-        line = table.readline()
-        if not line:
-            break
-        result_text = re.search(r'<big>/jboss/gvsBatchOutput/(SeattleSeqAnnotation131\.[\w\d_\.-]+\.txt)</big>',
-                                line)
-        if result_text:
-            result_file = result_text.group(1)
-            break
-
-    table.close()
-    if not result_file:
-        fatal_error('Could not find result file name in table page.')
-
-    return result_file
-
-
-def write_result_file(result_file, output_file):
-    """
-    Get gzipped result from server and write it to a file.
-
-    @todo: Maybe we should read/write buffered.
-    """
-    result_url = RESULT_URL_SCHEME.format(file=result_file)
-
-    debug('Result url: %s' % result_url)
-
-    # This is gzipped
-    result = get(result_url).read()
-
     try:
         output = open(output_file, 'wb')
     except IOError as (_, message):
         fatal_error(message)
 
-    output.write(result)
+    response = get(result_url)
+
+    # Buffered read/write
+    while True:
+        line = response.readline()
+        if not line:
+            break
+        output.write(line)
+
+    debug('Result written to: %s' % output_file)
+
+    response.close()
     output.close()
 
 
@@ -236,7 +223,6 @@ def get(url):
     Do a HTTP GET request and return file-like response object.
     """
     request = urllib2.Request(url)
-    #request.add_header('Cookie', 'JSESSIONID=xxx')
     response = urllib2.urlopen(request)
     return response
 
@@ -249,7 +235,6 @@ def post_multipart(url, parameters={}):
     register_openers()
     datagen, headers = multipart_encode(parameters)
     request = urllib2.Request(POST_URL, datagen, headers)
-    #request.add_header('Cookie', 'JSESSIONID=xxx')
     response = urllib2.urlopen(request)
     return response
 
@@ -268,14 +253,14 @@ if __name__ == '__main__':
         print """Annotate variants using SeattleSeq Annotation.
 
 Given a VCF file with variants, submit the file to the SeattleSeq Annotation
-web interface [1]. The annotation result is retrieved as gzipped tab-separated
-file.
+web interface [1]. The annotation result is retrieved as tab-separated file.
 
 Usage:
-  ./seattle_seq_annotation.py <input.vcf>
+  {command} <input.vcf>
 
-The result is written to disk as <input.vcf.annotation.gz>.
+The result is written to disk as <input.vcf.annotation>. Temporary data is
+written to <input.vcf.annotation.tmp> and removed afterwards.
 
-[1] http://gvs.gs.washington.edu/SeattleSeqAnnotation131/"""
+[1] {url}""".format(command=sys.argv[0], url=BASE_URL)
         sys.exit(1)
     seattle_seq_annotation(sys.argv[1])
